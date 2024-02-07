3891e4750b2ee39ff9201366cb91a510b485f0a1
vijay hitnalli <vijayhitnalli35@gmail.com>
2024-02-07 21:07:26 +0530

    Updated SecurityConfig

--- src/main/java/com/shoppingcart/security/CustomUserDetailService.java
+++ src/main/java/com/shoppingcart/security/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.shoppingcart.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.shoppingcart.exception.UserNotFoundException;
 import com.shoppingcart.repository.UserRepository;
@@ -10,6 +11,7 @@ import com.shoppingcart.repository.UserRepository;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@Service
 public class CustomUserDetailService implements UserDetailsService{
 
 	private UserRepository userRepository;
